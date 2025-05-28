246acb07eea4f5a82b69f86cdf44eaf002b570bf
Andrey Ayzenberg <eisenberg.nsk@gmail.com>
2025-05-28 16:56:01 +0200

    Swith tfstate storage to github artifact

--- backend.tf
+++ backend.tf
@@ -1,16 +1,6 @@
 terraform {
-  backend "http" {
-    address        = "https://api.github.com/repos/OWNER/REPO/contents/terraform.tfstate"
-    update_method  = "PUT"
-    lock_method    = "POST"
-    unlock_method  = "DELETE"
-    lock_address   = "https://api.github.com/repos/OWNER/REPO/contents/.terraform.tfstate.lock.info"
-    unlock_address = "https://api.github.com/repos/OWNER/REPO/contents/.terraform.tfstate.lock.info"
-    username       = "x-access-token"
-    password       = "GITHUB_TOKEN"
-    retry_max      = 3
-    retry_wait_min = 5
-    skip_cert_verification = false
+  backend "local" {
+    path = "terraform.tfstate"
   }
 
   required_providers {
