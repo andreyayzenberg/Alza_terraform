52d563c745302f97d4ff0d93fd0e36b5666249cb
Andrey Ayzenberg <eisenberg.nsk@gmail.com>
2025-05-28 13:35:55 +0200

    Add unlock possibility while running pipeline

--- backend.tf
+++ backend.tf
@@ -4,8 +4,12 @@ terraform {
     update_method  = "PUT"
     lock_method    = "POST"
     unlock_method  = "DELETE"
-    lock_address   = "https://api.github.com/repos/OWNER/REPO/contents/terraform.tfstate.lock"
-    unlock_address = "https://api.github.com/repos/OWNER/REPO/contents/terraform.tfstate.lock"
+    lock_address   = "https://api.github.com/repos/OWNER/REPO/contents/.terraform.tfstate.lock.info"
+    unlock_address = "https://api.github.com/repos/OWNER/REPO/contents/.terraform.tfstate.lock.info"
+    username       = "x-access-token"
+    password       = "GITHUB_TOKEN"
+    retry_max      = 3
+    retry_wait_min = 5
   }
 
   required_providers {
