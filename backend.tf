14ea534c6d1e5a9041e4dc563052a3e8affa8e0d
Andrey Ayzenberg <eisenberg.nsk@gmail.com>
2025-05-28 12:57:56 +0200

    Add remote tfstate strorage

--- backend.tf
+++ backend.tf
@@ -1,6 +1,11 @@
 terraform {
-  backend "local" {
-    path = "terraform.tfstate"
+  backend "http" {
+    address        = "https://api.github.com/repos/OWNER/REPO/contents/terraform.tfstate"
+    update_method  = "PUT"
+    lock_method    = "POST"
+    unlock_method  = "DELETE"
+    lock_address   = "https://api.github.com/repos/OWNER/REPO/contents/terraform.tfstate.lock"
+    unlock_address = "https://api.github.com/repos/OWNER/REPO/contents/terraform.tfstate.lock"
   }
 
   required_providers {
